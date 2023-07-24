43c95c119d0801298166b0fbb5ab52c90ec67240
Malearciuc Cosmin <125990875+VladMCiuc@users.noreply.github.com>
2023-07-24 16:52:36 +0300

    Add files via upload

---
+++ practica.ibm.spital-main/src/main/java/com/ibm/practica/spital/DTO/Employee.java
@@ -0,0 +1,16 @@
+package com.ibm.practica.spital.DTO;
+
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+
+public class Employee {
+
+    private String firstName;
+    private String lastName;
+    private String occupation;
+
+}
